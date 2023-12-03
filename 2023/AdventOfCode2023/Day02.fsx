2e9440be3ab4c9c709b96eedeb3a759c8a58dd15
solpyro <solpyro@gmail.com>
2023-12-03 13:20:11 +0200

    2023.2.1 - Tidy up

--- 2023/AdventOfCode2023/Day02.fsx
+++ 2023/AdventOfCode2023/Day02.fsx
@@ -3,11 +3,7 @@ open System.Collections.Generic
 
 let lines = System.IO.File.ReadAllLines("2023/AdventOfCode2023/inputs/02.txt")
 
-
-
-//parse
 type Game = {id:int;rounds:IDictionary<string, int>[]}
-
 let mapRounds(rounds:string[]) =
     rounds
     |> Array.map(fun round  -> 
@@ -15,34 +11,25 @@ let mapRounds(rounds:string[]) =
         |> Array.map(fun colour -> colour.Split(' ')[1], colour.Split(' ')[0] |> int)
         |> dict 
     )
-
 let games = 
     lines 
     |> Array.map(fun line -> line.Split([|"Game ";": ";"; "|], StringSplitOptions.RemoveEmptyEntries))
-// - games into rounds by ;
     |> Array.map(fun game -> {
         id= game[0] |> int; 
         rounds = mapRounds game[1..]
     }) 
 
-//part 1
-// - sum games that are possible if there are 12 red, 13 green, 14 blue
+let IsLessThan(round:IDictionary<string,int>, key:string, target:int) = 
+    match round.TryGetValue(key) with
+    | true, value -> value <= target
+    | _ -> true
+
 games
 |> Array.where(fun game -> 
-    Array.TrueForAll(
-        game.rounds, 
-        fun round -> 
-            match round.TryGetValue("red") with
-            | true, value -> value <= 12
-            | false, _ -> true
-            &&
-            match round.TryGetValue("green") with
-            | true, value -> value <= 13
-            | false, _ -> true
-            &&
-            match round.TryGetValue("blue") with
-            | true, value -> value <= 14
-            | false, _ -> true
+    Array.TrueForAll(game.rounds, fun round -> 
+        IsLessThan(round,"red",12) && 
+        IsLessThan(round,"green",13) && 
+        IsLessThan(round, "blue",14)
     )
 )
 |> Array.sumBy(fun game -> game.id)
