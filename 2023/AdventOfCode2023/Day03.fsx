46294b6de5f9fc313fcbb40a70ad7b059044e6d3
solpyro <solpyro@gmail.com>
2023-12-07 22:40:16 +0200

    2023.3.1 Scrapping the work done so far, since I got the wrong end of the stick

--- 2023/AdventOfCode2023/Day03.fsx
+++ 2023/AdventOfCode2023/Day03.fsx
@@ -8,20 +8,23 @@ let schematic =
 let height = Array2D.length1(schematic)-1
 let width = Array2D.length2(schematic)-1
 
-let neighbourValues(y, x, schematic:char[,]) =
-    [if y > 0 && x > 0 then yield (schematic[y-1, x-1] |> int)
-     if y > 0 then yield (schematic[y-1, x] |> int)
-     if y > 0 && x < width then yield (schematic[y-1, x+1] |> int)
-     if x < width then yield (schematic[y,x+1] |> int)
-     if y < height && x < width then yield (schematic[y+1,x+1] |> int)
-     if y < height then yield (schematic[y+1,x] |> int)
-     if y < height && x > 0 then yield (schematic[y+1,x-1] |> int)
-     if x > 0 then yield (schematic[y,x-1] |> int)]
+// TODO - Day 3 - reevaluate the input
+// let neighbourValues(y, x, schematic:char[,]) =
+//     [if y > 0 && x > 0 then yield (schematic[y-1, x-1] |> int)
+//      if y > 0 then yield (schematic[y-1, x] |> int)  
+//      if y > 0 && x < width then yield (schematic[y-1, x+1] |> int)
+//      if x < width then yield (schematic[y,x+1] |> int)
+//      if y < height && x < width then yield (schematic[y+1,x+1] |> int)
+//      if y < height then yield (schematic[y+1,x] |> int)
+//      if y < height && x > 0 then yield (schematic[y+1,x-1] |> int)
+//      if x > 0 then yield (schematic[y,x-1] |> int)]
 
-List.allPairs [0..height] [0..width]
-|> List.sumBy (fun (y, x) ->
-    match schematic[x,y] with
-    | '.' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> 0
-    | _ -> neighbourValues(y,x,schematic)  |> List.sum
-)
-|> printfn "Part 1: %i"
+// need to find all /\d+/ values that are adjacent to a symbol and sum them
+
+// List.allPairs [0..height] [0..width]
+// |> List.sumBy (fun (y, x) ->
+//     match schematic[x,y] with
+//     | '.' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> 0
+//     | _ -> neighbourValues(y,x,schematic)  |> List.sum
+// )
+// |> printfn "Part 1: %i"
