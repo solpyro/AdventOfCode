fd840ecbb739a8c93bd44f7f5335361c48dfcd7e
Stuart Kemp <stuart.kemp@if.fi>
2023-12-13 17:56:33 +0200

    2023.11.2 - Parkiny psudocode for now

--- 2023/AdventOfCode2023/Day11.fsx
+++ 2023/AdventOfCode2023/Day11.fsx
@@ -32,9 +32,9 @@ let expanded =
     |> ToCols
     |> Expand
 
-let galaxies =
-    List.allPairs [0..expanded.GetLength(0)-1] [0..expanded.GetLength(1)-1]
-    |> List.filter (fun (y, x) -> expanded[y,x] = '#')
+let FindGalaxies (image:char[,]) =
+    List.allPairs [0..image.GetLength(0)-1] [0..image.GetLength(1)-1]
+    |> List.filter (fun (y, x) -> image[y,x] = '#')
 
 let rec AllPairCombinations list =
     if List.length(list) = 2 then
@@ -43,7 +43,8 @@ let rec AllPairCombinations list =
         List.append (list.Tail |> (List.allPairs [list.Head])) (list.Tail |> AllPairCombinations)
 
 let pairs =
-    galaxies
+    expanded
+    |> FindGalaxies
     |> AllPairCombinations
 
 let GetDist ((y1,x1), (y2,x2)) =
@@ -53,5 +54,30 @@ pairs
 |> List.sumBy GetDist
 |> printfn "Part 1: %i"
 
-// input
+// let MillionExpand (arr:list<list<char>>)
+//     [
+//         for row in arr do
+//             if (row |> List.contains '#') then
+//                 yield row
+//             else
+//                 yield //'M' for row.Length
+//     ]
+//     |> array2D
+// let millionExpanded = 
+//     image
+//     |> ToCols
+//     |> MillionExpand
+//     |> ToCols
+//     |> MillionExpand
+
+// let millionPairs =
+//     millionExpanded
+//     |> FindGalaxies
+//     |> AllPairCombinations
+
+// let GetMillionDist ((y1,x1), (y2,x2)) =
+//     count xDiff  yDiff, adding 1_000_000 for every M also encountered
+
+// millionPairs
+// |> List.sumBy GetMillionDist
 // |> printfn "Part 2: %i"
